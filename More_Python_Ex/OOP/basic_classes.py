3194b06403b0de7c1ddacb72d7587cd1d65d08b2
Junuma-s2 <46939461+ManishaS24@users.noreply.github.com>
2021-04-15 00:42:23 +0530

    Update basic_classes.py

--- More_Python_Ex/OOP/basic_classes.py
+++ More_Python_Ex/OOP/basic_classes.py
@@ -1,8 +1,35 @@
 class Employee:
-    pass
+    def __init__(self, first_name, last_name,pay):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pay = pay
+        self.email = first_name + '.' + last_name + '@' + 'company.com'
 
-emp_1 = Employee()
+    def fullname(self):
+        return '{} {} '.format(self.first_name, self.last_name)
+
+"""emp_1 = Employee()
 emp_2 = Employee()
 
 print(emp_1)
-print(emp_2)
+print(emp_2) """
+
+""" emp_1.first_name = 'Manisha'
+emp_1.last_name = 'Sinha'
+emp_1.email = 'manisha.sinha@company.com'
+emp_1.pay = 70000
+
+emp_2.first_name = 'Ajay'
+emp_2.last_name = 'Dogra'
+emp_2.email = 'ajay.dogra@company.com'
+emp_2.pay = 75000
+
+print(emp_1.email, emp_2.email) """
+
+emp_1 = Employee('Manisha', 'Sinha', 60000)
+emp_2 = Employee('Amit', 'Mishra', 76000)
+
+print(emp_1.email, emp_2.email)
+
+print(emp_1.fullname(), emp_2.fullname())
+
