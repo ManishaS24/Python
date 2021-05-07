1d6627d60336825f20cfecfbca1a522cddb56826
Junuma-s2 <46939461+ManishaS24@users.noreply.github.com>
2021-05-07 21:45:56 +0530

    Update basic_class_vars.py

--- More_Python_Ex/OOP/basic_class_vars.py
+++ More_Python_Ex/OOP/basic_class_vars.py
@@ -1,10 +1,15 @@
 class Employee:
+    no_of_employees = 0
     amount_raise = 1.04
+
+
     def __init__(self, first_name, last_name,pay):
         self.first_name = first_name
         self.last_name = last_name
         self.pay = pay
         self.email = first_name + '.' + last_name + '@' + 'company.com'
+    
+    no_of_employees += 1
 
     def fullname(self):
         return '{} {} '.format(self.first_name, self.last_name)
@@ -13,9 +18,17 @@ class Employee:
         self.pay = int(self.pay * self.amount_raise)
         # self.pay = int(self.pay * Employee.amount_raise)
 
+print(Employee.no_of_employees)
 
 emp_1 = Employee('Manisha', 'Sinha', 60000)
 emp_2 = Employee('Amit', 'Mishra', 76000)
+
+print(Employee.no_of_employees)
+""" 
+print(emp_1.__dict__)
+print(emp_2.__dict__)
+print(Employee.__dict__) """
+
 
 print(emp_1.pay)
 emp_1.emp_raise()
@@ -29,6 +42,10 @@ print(Employee.amount_raise)
 
 
 emp_1.amount_raise = 1.06
+
+""" print(emp_1.__dict__)
+print(emp_2.__dict__)
+print(Employee.__dict__) """
 
 print(emp_1.amount_raise)
 print(emp_2.amount_raise)
