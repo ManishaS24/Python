31825f91cc2f33a14ec82aca6097eb118f8b0fa9
Junuma-s2 <46939461+ManishaS24@users.noreply.github.com>
2021-05-07 21:33:21 +0530

    new file added - class variables

---
+++ More_Python_Ex/OOP/basic_class_vars.py
@@ -0,0 +1,36 @@
+class Employee:
+    amount_raise = 1.04
+    def __init__(self, first_name, last_name,pay):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pay = pay
+        self.email = first_name + '.' + last_name + '@' + 'company.com'
+
+    def fullname(self):
+        return '{} {} '.format(self.first_name, self.last_name)
+    
+    def emp_raise(self):
+        self.pay = int(self.pay * self.amount_raise)
+        # self.pay = int(self.pay * Employee.amount_raise)
+
+
+emp_1 = Employee('Manisha', 'Sinha', 60000)
+emp_2 = Employee('Amit', 'Mishra', 76000)
+
+print(emp_1.pay)
+emp_1.emp_raise()
+print(emp_1.pay)
+
+Employee.amount_raise = 1.05
+
+print(emp_1.amount_raise)
+print(emp_2.amount_raise)
+print(Employee.amount_raise)
+
+
+emp_1.amount_raise = 1.06
+
+print(emp_1.amount_raise)
+print(emp_2.amount_raise)
+print(Employee.amount_raise)
+
