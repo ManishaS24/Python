3f32f8e3a8131e1785143fe19909dfbee7286e1f
Junuma-s2 <46939461+ManishaS24@users.noreply.github.com>
2021-06-15 00:45:38 +0530

    Update web_sraping.py
    Code modified with try-except block

--- More_Python_Ex/OOP/IntermediateOOP/web_sraping.py
+++ More_Python_Ex/OOP/IntermediateOOP/web_sraping.py
@@ -6,7 +6,8 @@ source = requests.get('http://coreyms.com').text
 soup = BeautifulSoup(source, 'lxml')
 # print(soup.prettify())
 
-for article in soup.find_all('article'):
+# # without try-except block
+""" for article in soup.find_all('article'):
     # print(article.prettify())
     
     headline = article.h2.a.text
@@ -27,4 +28,27 @@ for article in soup.find_all('article'):
     yt_link = f'https://youtube.com/watch?v={vid_id}'
     print('Video Link:',yt_link)
     
+    print() """
+
+# # with try-except block
+
+for article in soup.find_all('article'):
+    # print(article.prettify())
+    
+    headline = article.h2.a.text
+    print('Article Headline:',headline)
+    
+    summary = article.find('div', class_='entry-content').p.text
+    print('Article Summary:',summary)
+    
+    try:
+        vid_src = article.find('iframe', class_='youtube-player')['src']
+        vid_id = vid_src.split('/')[4].split('?')[0]
+
+        yt_link = f'https://youtube.com/watch?v={vid_id}'
+    
+    except Exception as e:
+        yt_link = None
+
+    print('Video Link:',yt_link)
     print()
