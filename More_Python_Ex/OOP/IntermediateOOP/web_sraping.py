8258cad766df93915c72f8d6c75db62ae0114117
Junuma-s2 <46939461+ManishaS24@users.noreply.github.com>
2021-06-15 00:31:15 +0530

    Update web_sraping.py

--- More_Python_Ex/OOP/IntermediateOOP/web_sraping.py
+++ More_Python_Ex/OOP/IntermediateOOP/web_sraping.py
@@ -4,16 +4,27 @@ import requests
 source = requests.get('http://coreyms.com').text
 
 soup = BeautifulSoup(source, 'lxml')
-
 # print(soup.prettify())
-article = soup.find('article')
 
-# print(article.prettify())
-
-headline = article.h2.a.text
-
-# print(headline)
-
-summary = article.find('div', class_='entry-content').p.text
-
-print(summary)
+for article in soup.find_all('article'):
+    # print(article.prettify())
+    
+    headline = article.h2.a.text
+    print('Article Headline:',headline)
+    
+    summary = article.find('div', class_='entry-content').p.text
+    print('Article Summary:',summary)
+    
+    if article.find('iframe', class_='youtube-player') is None:
+        continue
+    else:
+        vid_src = article.find('iframe', class_='youtube-player')['src']
+        #print(vid_src)
+    
+    vid_id = vid_src.split('/')[4].split('?')[0]
+    print(vid_id)
+    
+    yt_link = f'https://youtube.com/watch?v={vid_id}'
+    print('Video Link:',yt_link)
+    
+    print()
